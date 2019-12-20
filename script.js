45cb969f4186b67d09724c1f833415522d42dfc8
Ryan Borja <rcborja.school@gmail.com>
2019-12-20 13:21:15 -0600

    basic layout, variables, and click handlers

---
+++ script.js
@@ -0,0 +1,15 @@
+var searchInput = $("#searchTerm");
+var recordsAmountInput = $("#numberOfRecords");
+var startYearInput = $("#startYear");
+var endYearInput = $("#endYear");
+var searchBtn = $("#search");
+var getResultsBtn = $("#getResults");
+
+searchBtn.on("click", function() {
+ console.log("hello");
+});
+
+getResultsBtn.on("click", function() {
+    console.log("goodbye");
+
+});
