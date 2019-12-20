10592797250743a87955dd4d3d0cd174d71f5994
Ryan Borja <rcborja.school@gmail.com>
2019-12-20 13:48:20 -0600

    made Divs come up base on #records input

--- script.js
+++ script.js
@@ -4,12 +4,18 @@ var startYearInput = $("#startYear");
 var endYearInput = $("#endYear");
 var searchBtn = $("#search");
 var getResultsBtn = $("#getResults");
+var results = $("#resultsList");
 
-searchBtn.on("click", function() {
- console.log("hello");
+searchBtn.on("click", function () {
+    for (i = 0; i < recordsAmountInput.val(); i++) {
+        console.log("hello");
+        var article = $("<div>");
+        article.text("heello");
+        results.append(article);
+    }
 });
 
-getResultsBtn.on("click", function() {
+getResultsBtn.on("click", function () {
     console.log("goodbye");
 
 });
