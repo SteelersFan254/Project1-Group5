6edb9662690a683362fec06566bff06f58a3501b
Ryan Borja <rcborja.school@gmail.com>
2019-12-20 13:58:43 -0600

    got divs to appear on button click but as object object

--- script.js
+++ script.js
@@ -4,37 +4,38 @@ var recordsAmountInput = $("#numberOfRecords");
 var startYearInput = $("#startYear");
 var endYearInput = $("#endYear");
 var searchBtn = $("#search");
-var getResultsBtn = $("#getResults");
+var clearResultsBtn = $("#clearResults");
 var results = $("#resultsList");
 
+
 searchBtn.on("click", function () {
-    for (i = 0; i < recordsAmountInput.val(); i++) {
-        console.log("hello");
-        var article = $("<div>");
-        article.text("heello");
-        results.append(article);
-    }
+    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + searchTerm + "&api-key=EDG5iAEjuubmjv1ZmgJXjzGLQY0MLP8e"
+
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+        .then(function (response) {
+            console.log(response)
+    
+            var title = response
+            for (i = 0; i < recordsAmountInput.val(); i++) {
+                console.log("hello");
+                var article = $("<div>");
+                article.text(title);
+                results.append(article);
+            }
+        });
+        
+
+    
 });
 
-getResultsBtn.on("click", function () {
+clearResultsBtn.on("click", function () {
     console.log("goodbye");
 
 });
 
 
-var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + searchTerm + "&api-key=EDG5iAEjuubmjv1ZmgJXjzGLQY0MLP8e"
 
-searchBtn.on("click",function() {
-
-
-$.ajax({
-    url: queryURL,
-    method: "GET"
-})
-  .then(function(response) {
-    console.log(response)
-
-    var title = response
-
-  });
-});
