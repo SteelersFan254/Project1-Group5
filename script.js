ff8e833d7b10fcb0a23fd4b9e974b3497fac9f2a
Caleb Vincent <vincent_806@yahoo.com>
2020-01-06 14:02:49 -0600

    Made a succesful ajax call to work with

--- script.js
+++ script.js
@@ -8,34 +8,17 @@ var clearResultsBtn = $("#clearResults");
 var results = $("#resultsList");
 
 
-searchBtn.on("click", function () {
-    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + searchTerm + "&api-key=EDG5iAEjuubmjv1ZmgJXjzGLQY0MLP8e"
+// searchBtn.on("click", function () {
+// });
+
+var redirectHelper = "https://cors-ut-bootcamp.herokuapp.com/"; 
+var queryURL = "https://api.deezer.com/version/service/id/method/?parameters/search/artist/?q=eminem&index=0&limit=2&output=xml";
+var queryURL2= redirectHelper + "api.deezer.com/artist/27";
 
 
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    })
-        .then(function (response) {
-            console.log(response)
-    
-            var title = response
-            for (i = 0; i < recordsAmountInput.val(); i++) {
-                console.log("hello");
-                var article = $("<div>");
-                article.text(title);
-                results.append(article);
-            }
-        });
-        
-
-    
+$.ajax({
+  url: queryURL2,
+  method: "GET"
+}).then(function(response) {
+  console.log(response);
 });
-
-clearResultsBtn.on("click", function () {
-    console.log("goodbye");
-
-});
-
-
-
